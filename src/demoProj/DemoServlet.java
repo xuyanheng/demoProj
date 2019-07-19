1509edde693ce7f659813e4b3dcccaab36360911
erganzi <41982810+erganzi@users.noreply.github.com>
2019-07-19 15:29:52 +0800

    Update DemoServlet.java

--- src/demoProj/DemoServlet.java
+++ src/demoProj/DemoServlet.java
@@ -111,7 +111,7 @@ public class DemoServlet extends HttpServlet {
 			InetAddress netAddress = getInetAddress();
 			pw.println("<table><tr>"
 					+ "<td>Version</td>"
-					+ "<td>v1.5</td></tr>"
+					+ "<td>v11</td></tr>"
 					+ "<tr><td>Host IP</td>"
 					+ "<td>" + getHostIp(netAddress) + "</td></tr>"
 					+ "<tr><td>Host Name</td>"
